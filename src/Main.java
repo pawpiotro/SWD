a678465096103933b4a5b4d5ac216a0559df9a5f
Pawel Piotrowski <pawpiotro22@gmail.com>
2017-01-18 20:28:06 +0100

    Import xls (apache poi needed)

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@
  * Created by blank on 1/18/2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        ImportData imp = new ImportData();
+        imp.writeOut("","0wyniki.xls");
+    }
 }
